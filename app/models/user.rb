1cf5c6a6ef7f5916193db444c7bcd5701d5b543b
Monique Lee <moniquelee.7878@gmail.com>
2018-04-01 12:22:54 +1000

    add users controller and views, update model validations, add password encryption, sign-up/log-in/sign-out, add sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,8 @@
 #
 
 class User < ApplicationRecord
+  has_secure_password
   has_many :recipes
+  validates :name, :presence => true
+  validates :email, :presence => true, :uniqueness => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'pages#home'
+
+  resources :users
+
+  get '/users' => 'users#index'
+
+  get '/login' => 'session#new'        # Sign in form
+  post '/login' => 'session#create'    # Sign in action
+  delete '/login' => 'session#destroy' # Sign out
+
 end
