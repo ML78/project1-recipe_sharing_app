dd0a92f9876cd97dfd2758457029a612b17d7630
Monique Lee <moniquelee.7878@gmail.com>
2018-04-04 09:54:41 +1000

    add admin to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :text
-#  email      :text
-#  image      :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :text
+#  email           :text
+#  image           :text
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ApplicationRecord
